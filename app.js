5ba1accc4cf200356a46eea932e3c76708fc111a
sisssasd <isasabinka0000@gmail.com>
2024-11-21 09:09:11 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
@@ -16,20 +16,59 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
-function convertToWord(letter) {
-    if(letter === 'r') return "Rock";
-    if(letter === 'p') return "Paper";
-    return "Scissors";
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+  
 }
 
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+  
+}
+
+function draw(userChoice, computerChoice){
+
+}
+
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win(userChoice, computerChoice);
+            break;
+        case 'rp':  
+        case 'ps':
+        case 'sr':
+            lose(userChoice, computerChoice);
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            draw(userChoice, computerChoice);
+            break;                
+    }
+}
 
 
 function main() {
     rock_div.addEventListener('click', function(){
-        Gamepad('r');
+        game('r');
     })
 
     paper_div.addEventListener('click', function(){
-        
+        game('p');
+    })
+
+    scissors_div.addEventListener('click', function(){
+        game('s');
     })
 }
+
+main();
